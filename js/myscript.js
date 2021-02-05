8892b9631d08b8c083fbe4f682b764cb42e14e94
GlennSaestad <78589912+GlennSaestad@users.noreply.github.com>
2021-02-05 11:34:02 +0100

    Add files via upload

---
+++ js/myscript.js
@@ -0,0 +1,69 @@
+// THIS IS A COMMENT
+// start scripting - GOGOGO
+
+document.addEventListener('DOMContentLoaded', init);
+
+function init() {
+  let p1 = document.querySelector('div[data-file]');
+  let p2 = document.querySelector('#what-do-you-call');
+  let p3 = document.querySelector('#riser');
+  let p4 = document.querySelector('#siren');
+  let p5 = document.querySelector('#game-girls-play');
+  let p6 = document.querySelector('#lightworks2');
+  let p7 = document.querySelector('#name-of');
+  let p8 = document.querySelector('#sweeps2');
+  let p9 = document.querySelector('#listen-up2');
+  let p10 = document.querySelector('#yeah-uh');
+  p1.addEventListener('click', play);
+  p1.addEventListener('click', start);
+  p1.addEventListener('click', rotate);
+
+  p2.addEventListener('click', play);
+  p3.addEventListener('click', play);
+  p4.addEventListener('click', play);
+  p5.addEventListener('click', play);
+  p6.addEventListener('click', play);
+  p7.addEventListener('click', play);
+  p8.addEventListener('click', play);
+  p9.addEventListener('click', play);
+  p10.addEventListener('click', play);
+}
+
+
+function play(ev){
+           let p = ev.currentTarget;
+           ev.preventDefault();
+
+           let fn = p.getAttribute('data-file');
+           let src = './media/' + fn + '.mp3';
+           // if( SOUNDS[fn] ){
+           //     SOUNDS[fn].pause();
+           //     SOUNDS[fn] = null;
+           // }
+
+           console.log(src);
+
+           let audio = document.createElement('audio');
+           audio.src = src;
+           audio.volume = 0.4;
+           audio.play();
+}
+
+function start() {
+  var steps = document.getElementsByClassName("metronomeStep"),
+        len = steps !== null ? steps.length : 0,
+        i = 0;
+    for(i; i < len; i++) {
+        steps [i].className += " animateStep";
+        console.log("added");
+    }
+}
+    function rotate() {
+      var steps = document.getElementsByClassName("startDiv"),
+            len = steps !== null ? steps.length : 0,
+            i = 0;
+        for(i; i < len; i++) {
+            steps [i].className += " startAnimate";
+            console.log("added rotation");
+        }
+}
